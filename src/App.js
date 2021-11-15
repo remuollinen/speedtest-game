54f951c86ffe517a556007233776942d63bd62da
Remu Ollinen <remu.ollinen@gmail.com>
2021-11-15 11:01:07 +0200

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Circle from "./components/Circle";
 
 export default class App extends Component {
 	render() {
@@ -7,8 +8,14 @@ export default class App extends Component {
 			<div className="App">
 				<h1>Speedtest</h1>
 				<h3>
-					Your score <span>0</span>
+					Your score: <span>0</span>
 				</h3>
+				<div className="circle-container">
+					<Circle />
+					<Circle />
+					<Circle />
+					<Circle />
+				</div>
 				<button>Start</button>
 				<button>Stop</button>
 			</div>
